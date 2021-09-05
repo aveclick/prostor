740ad839ea77fa3cdfdbd1aa0db5f3d22df133c9
avamay <avamay481516@gmail.com>
2021-09-06 02:01:20 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
-  $('.FQA__item-title').click(function(event) {
+  $('.accord__title').click(function(event) {
     $(this).next().slideToggle(300);
+  });
+  $('.accord__title-1').click(function(event) {
+    $('.accord__text-1').slideToggle(300);
+  });
+  $('.accord__title-2').click(function(event) {
+    $('.accord__text-2').slideToggle(300);
+  });
+  $('.accord__title-3').click(function(event) {
+    $('.accord__text-3').slideToggle(300);
   });
 });
